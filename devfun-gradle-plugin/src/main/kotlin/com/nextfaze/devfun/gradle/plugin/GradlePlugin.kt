7754255ef0256e5d4a1d26a190cb93dabb9723a7
Alex Waters <awaters@nextfaze.com>
2018-09-26 15:19:41 +0930

    Consolidate gradle-plugin plugin types

--- devfun-gradle-plugin/src/main/kotlin/com/nextfaze/devfun/gradle/plugin/GradlePlugin.kt
+++ devfun-gradle-plugin/src/main/kotlin/com/nextfaze/devfun/gradle/plugin/GradlePlugin.kt
@@ -1,17 +1,52 @@
 package com.nextfaze.devfun.gradle.plugin
 
-import com.android.build.gradle.BaseExtension
-import com.google.auto.service.AutoService
 import com.nextfaze.devfun.compiler.*
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.ExtensionContainer
-import org.gradle.api.tasks.compile.AbstractCompile
-import org.jetbrains.kotlin.gradle.internal.KaptVariantData
-import org.jetbrains.kotlin.gradle.plugin.*
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.io.File
-import javax.xml.parsers.DocumentBuilderFactory
+
+/**
+ * Gradle DSL for configuring DevFun.
+ *
+ * Values provided via Gradle plugin config can/will be overridden by values provided via. APT options.
+ *
+ * @see DevFunProcessor
+ * @see PACKAGE_SUFFIX
+ * @see PACKAGE_ROOT
+ * @see PACKAGE_OVERRIDE
+ */
+open class DevFunExtension {
+    /**
+     * Sets the package suffix for the generated code. _(default: `devfun_generated`)_
+     *
+     * This is primarily for testing purposes to allow multiple generations in the same classpath.
+     * - If this is null (unset) [PACKAGE_SUFFIX_DEFAULT] will be used.
+     * - If this is empty the suffix will be omitted.
+     *
+     * Final output package will be: [packageRoot].`<variant?>`.[packageSuffix]
+     *
+     * `<variant?>` will be omitted if both `packageRoot` and `packageSuffix` are provided.
+     */
+    var packageSuffix: String? = null
+
+    /**
+     * Sets the package root for the generated code. _(default: `<application package>`)_
+     *
+     * Attempts will be made to auto-detect the project package by using the class output directory and known/standard
+     * relative paths to various build files, but if necessary this option can be set instead.
+     *
+     * Final output package will be: [packageRoot].`<variant?>`.[packageSuffix]
+     *
+     * `<variant?>` will be omitted if both `packageRoot` and `packageSuffix` are provided.
+     */
+    var packageRoot: String? = null
+
+    /**
+     * Sets the package for the generated code. _(default: `<none>`)_
+     *
+     * This will override [packageRoot] and [packageSuffix].
+     */
+    var packageOverride: String? = null
+}
 
 /**
  * The DevFun Gradle plugin. Allows use of the script configuration DSL.
@@ -23,153 +58,3 @@ class DevFunGradlePlugin : Plugin<Project> {
         project.extensions.create("devFun", DevFunExtension::class.java)
     }
 }
-
-/**
- * The DevFun Kotlin Gradle plugin. Configures the KAPT options.
- *
- * Attempts to automatically determine the application package and build variant.
- * Also passes though the script configuration options.
- *
- * @see DevFunExtension
- */
-@AutoService(KotlinGradleSubplugin::class)
-class DevFunKotlinGradlePlugin : KotlinGradleSubplugin<AbstractCompile> {
-    override fun getCompilerPluginId() = DEVFUN_GROUP_NAME
-    override fun getPluginArtifact() = SubpluginArtifact(DEVFUN_GROUP_NAME, DEVFUN_ARTIFACT_NAME, DEVFUN_VERSION_NAME)
-
-    override fun isApplicable(project: Project, task: AbstractCompile): Boolean {
-        if (project.plugins.findPlugin(DevFunGradlePlugin::class.java) == null) return false
-        if (task !is KotlinCompile) return false // we generate Kotlin code so definitely need this
-
-        val androidExt = project.extensions.android ?: run {
-            project.logger.error("Failed to find Android BaseExtension. Please ensure the devfun plugin is declared *after* the android plugin.")
-            return false
-        }
-        val kotlinExt = project.extensions.kapt ?: run {
-            project.logger.error("Failed to find KaptExtension. Please ensure the devfun plugin is declared *after* the kotlin-kapt plugin.")
-            return false
-        }
-        val devFunExt = project.extensions.devFun ?: run {
-            project.logger.error("Failed to find DevFunExtension. Please ensure the devfun plugin has been applied.")
-            return false
-        }
-
-        val appPackage = getApplicationPackage(project, androidExt) ?: run {
-            project.logger.warn("Unable to determine application package. Using DevFun package $DEVFUN_GROUP_NAME")
-            DEVFUN_GROUP_NAME
-        }
-        val variant = run {
-            val taskName = task.name
-            kotlinCompileTaskNameRegex.find(taskName)?.groupValues?.getOrNull(1)?.decapitalize() ?: run {
-                project.logger.warn("Failed to identify variant name from task name. Using 'main'")
-                "main"
-            }
-        }
-
-        kotlinExt.arguments {
-            fun nonNullArg(name: String, value: String?) = value?.let { arg(name, it) }
-
-            nonNullArg(APPLICATION_PACKAGE, appPackage)
-            nonNullArg(APPLICATION_VARIANT, variant)
-
-            nonNullArg(EXT_PACKAGE_SUFFIX, devFunExt.packageSuffix)
-            nonNullArg(EXT_PACKAGE_ROOT, devFunExt.packageRoot)
-            nonNullArg(EXT_PACKAGE_OVERRIDE, devFunExt.packageOverride)
-        }
-
-        return true
-    }
-
-    override fun apply(
-        project: Project,
-        kotlinCompile: AbstractCompile,
-        javaCompile: AbstractCompile?,
-        variantData: Any?,
-        androidProjectHandler: Any?,
-        kotlinCompilation: KotlinCompilation?
-    ): List<SubpluginOption> {
-        //
-        // TODO? Currently the kotlinExt value is not invoked for the first variant for some reason (which is typically the debug variant)
-        //
-        // Thus we're doing all our work in the isApplicable block instead (which is not ideal since we have to guess
-        // the name from the task name rather than from the KaptVariantData object).
-        //
-        if (true) return emptyList()
-
-        val androidExt = project.extensions.android ?: run {
-            project.logger.error("Failed to find Android BaseExtension. Please ensure the devfun plugin is declared *after* the android plugin.")
-            return emptyList()
-        }
-        val kotlinExt = project.extensions.kapt ?: run {
-            project.logger.error("Failed to find KaptExtension. Please ensure the devfun plugin is declared *after* the kotlin-kapt plugin.")
-            return emptyList()
-        }
-        val devFunExt = project.extensions.devFun ?: run {
-            project.logger.error("Failed to find DevFunExtension. Please ensure the devfun plugin has been applied.")
-            return emptyList()
-        }
-
-        val appPackage = getApplicationPackage(project, androidExt) ?: run {
-            project.logger.warn("Unable to determine application package. Using DevFun package $DEVFUN_GROUP_NAME")
-            DEVFUN_GROUP_NAME
-        }
-        val variant = if (variantData is KaptVariantData<*>) {
-            variantData.name
-        } else {
-            val taskName = kotlinCompile.name
-            project.logger.warn(
-                """Variant data was not KaptVariantData<*> (variantData=$variantData).
-                |Guessing variant name from kotlin compile $kotlinCompile""".trimMargin()
-            )
-
-            kotlinCompileTaskNameRegex.find(taskName)?.groupValues?.getOrNull(1)?.decapitalize() ?: run {
-                project.logger.warn("Failed to identify variant name from task name. Using 'main'")
-                "main"
-            }
-        }
-
-        kotlinExt.arguments {
-            fun nonNullArg(name: String, value: String?) = value?.let { arg(name, it) }
-
-            nonNullArg(APPLICATION_PACKAGE, appPackage)
-            nonNullArg(APPLICATION_VARIANT, variant)
-
-            nonNullArg(EXT_PACKAGE_SUFFIX, devFunExt.packageSuffix)
-            nonNullArg(EXT_PACKAGE_ROOT, devFunExt.packageRoot)
-            nonNullArg(EXT_PACKAGE_OVERRIDE, devFunExt.packageOverride)
-        }
-
-        return emptyList()
-    }
-}
-
-private const val DEVFUN_GROUP_NAME = "com.nextfaze.devfun"
-private const val DEVFUN_ARTIFACT_NAME = "devfun-gradle-plugin"
-
-// TODO this is just annoying - package with JAR or something so we can set at build time from build state and get it dynamically at run time
-private const val DEVFUN_VERSION_NAME = "1.3.0-SNAPSHOT"
-
-private val ExtensionContainer.android get() = findByType(BaseExtension::class.java)
-private val ExtensionContainer.kapt get() = findByType(KaptExtension::class.java)
-private val ExtensionContainer.devFun get() = findByType(DevFunExtension::class.java)
-
-private val kotlinCompileTaskNameRegex = Regex("compile(.*)Kotlin")
-
-private fun getApplicationPackage(project: Project, android: BaseExtension): String? {
-    fun File.parseXml() = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(this)
-
-    val manifestFile = android.mainSourceSet.manifest.srcFile
-    return try {
-        manifestFile.parseXml().documentElement.getAttribute("package")
-            .also {
-                if (it.isNullOrBlank()) {
-                    project.logger.warn("Application package name is not present in the manifest file (${manifestFile.absolutePath})")
-                }
-            }
-    } catch (t: Throwable) {
-        project.logger.warn("Failed to parse manifest file (${manifestFile.absolutePath})", t)
-        null
-    }
-}
-
-private val BaseExtension.mainSourceSet get() = sourceSets.getByName("main")
