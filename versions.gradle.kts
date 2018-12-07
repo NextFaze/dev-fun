c17e2108d781db158ec37afa9563f613c931e735
Alex Waters <awaters@nextfaze.com>
2018-12-07 14:58:01 +1030

    Update to Kotlin 1.3.11

--- versions.gradle.kts
+++ versions.gradle.kts
@@ -1,5 +1,5 @@
 val buildSrcKotlinVersion: String by extra(findProperty("buildSrc.kotlin.version")?.toString() ?: embeddedKotlinVersion)
 
-val kotlinVersion by extra("1.3.10")
+val kotlinVersion by extra("1.3.11")
 val dokkaVersion by extra("0.9.17")
 val agpVersion by extra("3.2.1")
